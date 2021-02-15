43f3bcfbd93d67ccdc095d9fd2166ac5b307c919
NickZt <nickmezon@gmail.com>
2021-02-15 14:13:53 +0200

    fix old i18n

--- lib/foreground/base_widgets/MiscWidgets.dart
+++ lib/foreground/base_widgets/MiscWidgets.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 mixin MiscWidgets {
   launchURL(String url) async {
     if (await canLaunch(url)) {
